4cdc4b534122f7e9dee6762b2115cff5c2b5d6c4
Kuba Sobczyński <jak.sobczynski@gmail.com>
2022-05-17 14:54:50 +0200

    poprawienie bledu

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e)=>{
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('/weather?addres='+location).then((response)=> {
+    fetch('/weather?address='+location).then((response)=> {
     response.json().then((data) => {
         if(data.error){
             console.log(data.error)
