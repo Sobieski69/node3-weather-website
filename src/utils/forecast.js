ed48dc56a7db45ef06574c69e2bc8cb4838ae76b
Kuba Sobczyński <jak.sobczynski@gmail.com>
2022-05-17 15:10:34 +0200

    added humidity %

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longtitude, callback)=>{
             callback('unable to load location', undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. And it feels like ' + body.current.feelslike + ' degrees.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. And it feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + "%")
         }
 })
 }
