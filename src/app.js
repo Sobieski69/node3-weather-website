922fceebfc888384e2602a31d2af3b0af6556a0a
Kuba Sobczyński <jak.sobczynski@gmail.com>
2022-05-17 14:46:33 +0200

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
-
+const port = process.env.PORT|| 3000
 //Define paths fore express config
 const publicDierctioryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -94,6 +94,6 @@ app.get('*', (req, res)=>{
         message: 'Page not found'
     })
 })
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port)
 })
